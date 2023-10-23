05a13a0c13f8c4f2be850f260eafb8ca3154517f
Ewenar <pomegranatetohome55@hotmail.com>
2023-10-23 15:22:16 +0300

    Commit3

--- app/src/main/java/com/travel/gittest/MainActivity.kt
+++ app/src/main/java/com/travel/gittest/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hellosdsc msdmcnscsc $name!",
         modifier = modifier
     )
 }
